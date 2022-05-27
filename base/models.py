ce62b35356eb998c8b46f5a2e8899a8aab68ec58
ibs12 <ibrahimallahb@gmail.com>
2022-05-27 14:30:39 -0400

    Add Studybud project

---
+++ base/models.py
@@ -0,0 +1,22 @@
+from enum import auto
+from django.db import models
+
+# Create your models here.
+
+class Room(models.Model):
+    #host = 
+    #topic =
+
+    name = models.CharField(max_length=200)
+    description = models.TextField(null = True, blank=True)
+    #participants = 
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.name
+
+class Message(modles.Model):
+    #user =
+    room = model.ForeignKey
